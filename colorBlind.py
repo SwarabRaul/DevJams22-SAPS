4da8c67101268e6411b047b86427cb4f9ae9b187
Pritika Kannapiran <pritika@Pritikas-MacBook-Air.local>
2022-09-30 21:33:10 +0530

    Image

--- colorBlind.py
+++ colorBlind.py
@@ -1,1 +1,5 @@
-import numpy as np
+import cv2
+
+image=cv2.imread("//Users//pritika//Documents//VIT//Projects//GitHub//DevJams22-SAPS//tiger.jpeg")
+image1=cv2.cvtColor(image, cV2.COLOR_BGR2GRAY)
+cv2.imwrite("//Users//pritika//Documents//VIT//Projects//GitHub//DevJams22-SAPS//Gray_tiger.jpeg", image1)
